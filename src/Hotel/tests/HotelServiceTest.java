cf53663892e168092f8def1fae3a4afc08d6aa60
Philipp-Teodor Gyarmati <philipp.gyarmati@gmail.com>
2024-12-10 15:56:37 +0200

    Solved issue regarding project's ability to solve tests, now moving to a branch separate from Master

---
+++ src/Hotel/tests/HotelServiceTest.java
@@ -0,0 +1,127 @@
+package Hotel.tests;
+
+import org.junit.jupiter.api.Test;
+
+class HotelServiceTest {
+
+    @Test
+    void checkDirtyRooms() {
+        assert(1==1);
+    }
+
+    @Test
+    void cleanRoom() {
+    }
+
+    @Test
+    void createClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void getAvailableRooms() {
+    }
+
+    @Test
+    void sortRoomCustomerByUntilDate() {
+    }
+
+    @Test
+    void getAllRoomsOfACustomer() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getNextAvailableId() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void getEmployeeTypeString() {
+    }
+
+    @Test
+    void getEmployeeTypeObject() {
+    }
+
+    @Test
+    void getManagersManagedDepartmentType() {
+    }
+
+    @Test
+    void showAllDepartments() {
+    }
+
+    @Test
+    void sortEmployeesBySalary() {
+    }
+
+    @Test
+    void createDepartment() {
+    }
+
+    @Test
+    void deleteDepartment() {
+    }
+
+    @Test
+    void updateDepartment() {
+    }
+
+    @Test
+    void getAllDepartments() {
+    }
+
+    @Test
+    void getDepartmentEmployees() {
+    }
+
+    @Test
+    void getAvgSalaryOfADepartment() {
+    }
+
+    @Test
+    void getDepartmentsWithOverGivenNumberAvgSalary() {
+    }
+
+    @Test
+    void createRoomCustomer() {
+    }
+
+    @Test
+    void searchRoomCustomerByCustomer() {
+    }
+
+    @Test
+    void deleteRoomCustomer() {
+    }
+
+    @Test
+    void updateRoomCustomer() {
+    }
+}
